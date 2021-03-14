fd6c49ed0901be3e23a78709d4cd870030559a73
dueToLife <wkp372874136@mail.ustc.edu.cn>
2021-03-14 23:37:45 +0800

    test

--- report.py
+++ report.py
@@ -24,7 +24,7 @@ class Report(object):
             getform = session.get("https://weixine.ustc.edu.cn/2020")
             retrycount = retrycount - 1
             if getform.url != "https://weixine.ustc.edu.cn/2020/home":
-                print("Login Failed! Retry...")
+                print("Login Failed! Retrying...")
             else:
                 print("Login Successful!")
                 loginsuccess = True
@@ -47,14 +47,14 @@ class Report(object):
             'content-type': 'application/x-www-form-urlencoded',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'referer': 'https://weixine.ustc.edu.cn/2020/',
+            'referer': 'https://weixine.ustc.edu.cn/2020/home',
             'accept-language': 'zh-CN,zh;q=0.9',
             'Connection': 'close',
-            'cookie': 'PHPSESSID=' + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
+            'cookie': "PHPSESSID=" + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
         }
 
         url = "https://weixine.ustc.edu.cn/2020/daliy_report"
-        session.post(url, data=data, headers=headers)
+        resp=session.post(url, data=data, headers=headers)
         data = session.get("https://weixine.ustc.edu.cn/2020").text
         soup = BeautifulSoup(data, 'html.parser')
         pattern = re.compile("202[0-9]-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
@@ -78,9 +78,7 @@ class Report(object):
         return flag
 
     def login(self):
-        # 用http协议无法登录
-        url = "https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin"
-        #需要showCode这个表项，另两个省略表项不影响
+        url = "https://passport.ustc.edu.cn/login?service=http%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin"
         data = {
             'model': 'uplogin.jsp',
             'service': 'https://weixine.ustc.edu.cn/2020/caslogin',
@@ -91,8 +89,8 @@ class Report(object):
             'button': '',
         }
         session = requests.Session()
-        session.post(url=url, data=data)
-        #print(r.url)
+        session.post(url, data=data)
+
         print("login...")
         return session
 
@@ -105,7 +103,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     autorepoter = Report(stuid=args.stuid, password=args.password, data_path=args.data_path)
     count = 5
-    print("kjk", args.stuid,count)
     while count != 0:
         ret = autorepoter.report()
         if ret != False:
