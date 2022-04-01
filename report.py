50aa0ae6caa5117ea4b4bf32dd84e5864e43e2d4
NSF-Nagisa <1186718712@qq.com>
2022-04-01 12:08:26 +0800

    use environ variables

--- report.py
+++ report.py
@@ -51,12 +51,12 @@ class Report(object):
                 data = f.read()
                 data = json.loads(data)
                 data["_token"] = token
-                # data["jinji_lxr"] = os.environ["CONTACTNAME"]
-                # data["jinji_guanxi"] = os.environ["CONTACTRALASHIP"]
-                # data["jiji_mobile"] = os.environ["CONTACTPHONE"]
-                data["jinji_lxr"] = "kk"
-                data["jinji_guanxi"] = "kk"
-                data["jiji_mobile"] = "18189700666"
+                data["jinji_lxr"] = os.environ["CONTACTNAME"]
+                data["jinji_guanxi"] = os.environ["CONTACTRALASHIP"]
+                data["jiji_mobile"] = os.environ["CONTACTPHONE"]
+                # data["jinji_lxr"] = "kk"
+                # data["jinji_guanxi"] = "kk"
+                # data["jiji_mobile"] = "18189700666"
             
             headers = {
                 'authority': 'weixine.ustc.edu.cn',
